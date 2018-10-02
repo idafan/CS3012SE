2ff50ee9888a9773968746f076845fa035cb2d54
Ida Fantenberg Niklasson <31707115+idafan@users.noreply.github.com>
2018-10-02 23:15:38 +0100

    Add testbinarytree file

---
+++ testbinarytree.py
@@ -0,0 +1,14 @@
+import unittest
+import binarytreefile
+
+class TestBTree(unittest.TestCase):
+
+    def test_root(self):
+        return 0
+        #Create a node, test that the node now have a value
+
+
+    def test_addNode(self):
+        #Add a second node, test if it gets to the right side
+        return 0
+        
