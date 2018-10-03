e6c1a35358fcb340d2bd3a05f772760f80b0a599
Ida Fantenberg Niklasson <inik@kth.se>
2018-10-03 01:06:56 +0100

    addNode works for several number
    I've added the numbers as characters which did that the if-statement didn't work.

--- binarytreefile.py
+++ binarytreefile.py
@@ -26,21 +26,21 @@ def addNode(node, newnode):
     if node == None:
         node  = Node(newnode)
 
-    elif newnode < node.value:
+    elif newnode > node.value:
         #If the newnode is greater than the node there is two cases
         #If the node.greater is None, the newnode will be added as a node there
         #If there is a value in node.greater, that value there will be compared with the newnode
         if node.greater == None:
             node.greater = Node(newnode)
         else:
-            addNode(node.greater, newnode)
+            node.greater = addNode(node.greater, newnode)
 
     else:
         #The same two cases as above
         if node.less == None:
             node.less = Node(newnode)
         else:
-            addNode(node.less, newnode)
+            node.less = addNode(node.less, newnode)
 
 
     return node #The node needs to be returned to be saved in self.root
--- test_binarytreefile.py
+++ test_binarytreefile.py
@@ -6,83 +6,24 @@ class TestRoot(unittest.TestCase):
     def test_root(self):
         #Create a root, test that the node now have a value
         Bintree = BinaryTree()
-        Bintree.add("10")
+        Bintree.add(10)
         root = Bintree.root
         self.assertIsNotNone(root)
 
     def test_nodes(self):
         Bintree = BinaryTree()
-        Bintree.add("10")
-        Bintree.add("8")
-        Bintree.add("15")
-        self.assertEqual(Bintree.root.value, "10")
-        self.assertEqual(Bintree.root.less.value, "8")
-        self.assertEqual(Bintree.root.greater.value, "15")
+        Bintree.add(10)
+        Bintree.add(8)
+        Bintree.add(15)
+        Bintree.add(2)
+        Bintree.add(12)
+        self.assertEqual(Bintree.root.value, 10)
+        self.assertEqual(Bintree.root.less.value, 8)
+        self.assertEqual(Bintree.root.greater.value, 15)
+        self.assertEqual(Bintree.root.less.less.value, 2)
+        self.assertEqual(Bintree.root.greater.less.value, 12)
+        
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-##F
-##======================================================================
-##FAIL: test_root (__main__.TestRoot)
-##----------------------------------------------------------------------
-##Traceback (most recent call last):
-##  File "C:\Users\idafn\Documents\GitHub\CS3012SE\test_binarytreefile.py", line 11, in test_root
-##    self.assertIsNotNone(root)
-##AssertionError: unexpectedly None
-##
-##----------------------------------------------------------------------
-##Ran 1 test in 0.008s
-##
-##FAILED (failures=1)
-##
-    
-"""added return node in addNode()"""
-
-##
-##.
-##----------------------------------------------------------------------
-##Ran 1 test in 0.011s
-##
-##OK
-
-##E.
-##======================================================================
-##ERROR: test_nodes (__main__.TestRoot)
-##----------------------------------------------------------------------
-##Traceback (most recent call last):
-##  File "C:\Users\idafn\Documents\GitHub\CS3012SE\test_binarytreefile.py", line 16, in test_nodes
-##    Bintree.add("8")
-##  File "C:\Users\idafn\Documents\GitHub\CS3012SE\binarytreefile.py", line 18, in add
-##    self.root = addNode(self.root, node)
-##  File "C:\Users\idafn\Documents\GitHub\CS3012SE\binarytreefile.py", line 29, in addNode
-##    elif newnode > node:
-##TypeError: '>' not supported between instances of 'str' and 'Node'
-##
-##----------------------------------------------------------------------
-##Ran 2 tests in 0.019s
-##
-##FAILED (errors=1)
-##
-##
-
-"""changed ">" to "<" in the if-statement, 8 is added to the tree"""
-
-##E.
-##======================================================================
-##ERROR: test_nodes (__main__.TestRoot)
-##----------------------------------------------------------------------
-##Traceback (most recent call last):
-##  File "C:\Users\idafn\Documents\GitHub\CS3012SE\test_binarytreefile.py", line 20, in test_nodes
-##    self.assertEqual(Bintree.root.greater.value, "15")
-##AttributeError: 'NoneType' object has no attribute 'value'
-##
-##----------------------------------------------------------------------
-##Ran 2 tests in 0.010s
-##
-##FAILED (errors=1)
-
-"""it doesn't work to add 15 to the tree, the data in Bintree.root.greater is None"""
