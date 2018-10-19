24797df9d8bcd53fee64a5c49dcd22ba19c7eef2
Ida Fantenberg Niklasson <inik@kth.se>
2018-10-19 04:05:26 +0100

    LCA for DAG doesn't work

--- DAGfile.py
+++ DAGfile.py
@@ -4,26 +4,53 @@ class Node():
 
     def __init__(self, value = None, arrow = None ):
         self.value = value #The node's value
-        self.arrow = arrow #This will be a list of the objects that the node are pointing at
+        self.arrow = [arrow] #This will be a list of the objects that the node are pointing at
 
 
 class DAGclass():
 
     def __init__(self):
-        """graph with a list of all the vertices and edges"""
+        """graph with a list of all the nodes and arrows/vertices and edges"""
         self.graph = None
         
 
     def add(self, node, arrow = None):
         """adds a node to the graph"""
-
+##        print(node)
         self.graph = addNode(self.graph, node, arrow)
+
+
+    def addArrow(self, newnode, arrow):
+        """Add an arow between two already exisiting nodes"""
+        contain1 = self.contains(newnode)
+        contain2 = self.contains(arrow)
+        
+        if contain1 and contain2:
+            #Both nodes must already be in the graph
+            
+            for node in self.graph:
+                if node.value == newnode:
+                    for arrowNode in self.graph:
+                        if arrowNode.value == arrow:
+                            node.arrow.append(arrowNode)
+                            #Something here is wrong I think, the other node that the node is pointing to needs to be append as an object in the list of arrows. 
+
+        elif contain1 is False and contain2:
+            return str(value1) + " is not in the graph"
+
+        elif contain2 is False and contain1:
+            return str(value2) + " is not in the graph"
+        
+        else:
+            return str(value1) + " and " + str(value2) + " are not in the graph"
+        
 
     
     def contains(self, value):
         """checks if the value is in the graph"""
         if self.graph == None:
             sys.exit("There are no values in the graph")
+            #Kan inte använda sys.exit iom unittesting, try or except?????
             
         else:
             return contain(self.graph, value)
@@ -33,6 +60,9 @@ class DAGclass():
         """checks that the values for LCA are in the graph"""
         contain1 = self.contains(value1)
         contain2 = self.contains(value2)
+
+##        for node in self.graph:
+##            print(node.value)
         
         if contain1 and contain2:
             #if both values are in the graph the LCA is checked
@@ -64,21 +94,21 @@ def addNode(graph, newnode, arrow):
             sys.exit("The graph is empty, the node can't have a parent")
             
     else:
-        for vertex in graph:
-            if vertex.value == arrow:
-                parent = vertex
+        for node in graph:
+            if node.value == arrow:
+                parent = node
                 break
             
-        node = Node(newnode,vertex)
+        node = Node(newnode,parent)
         graph.append(node)
 
     return graph
 
 
 def contain(graph, value):
-    """Goes through the vertices to see that the value is in the graph"""
-    for vertex in graph:
-        if vertex.value == value:
+    """Goes through the nodes to see that the value is in the graph"""
+    for node in graph:
+        if node.value == value:
             return True
 
     return False
@@ -89,46 +119,64 @@ def lowest(graph, value1, value2):
     path1 = []
     path1.append(value1)
     path1 = pathDAG(graph, value1, path1)
+    print(path1)
 
     path2 = []
     path2.append(value2)
     path2 = pathDAG(graph, value2, path2)
+    print(path2)
 
     for node1 in path1:
         for node2 in path2:
             if node1 == node2:
-                return node1       
+                break
+        else:
+            continue #executed if inner loop do not breaks
+        break #executed if inner loop do break
+    return node1
          
 
 def pathDAG(graph, value, path):
     """Find the path via recursive function.
     Starts with adding the value, then all the nodes to the root in the list path"""
 
-    for vertex in graph:
-        if vertex.value == value:
-            if vertex.arrow == None:
-                break
-            else:
-                path.append(vertex.arrow.value)
-                pathDAG(graph, vertex.arrow.value, path)
+    for node in graph:
+##        print("1 " + node.value)
+        if node.value == value:
+            for vertex in node.arrow:
+##                print(len(node.arrow))
+##                print(vertex)
+                if vertex == None:
+##                    print("Noneeeee")
+                    break
+##                elif len(node.arrow) >= 2:
+##
+##                    for arrow in node.arrow:
+##                        print("hej: " + arrow.value)
+##                        path.append(arrow.value)
+##                        
+##                        pathDAG(graph, arrow.value, path)
+                    
+                else:
+                    for arrow in node.arrow:
+##                        print("2 " + arrow.value)
+                        path.append(arrow.value)
+                        pathDAG(graph, arrow.value, path)
+##                        print("after")
+##                    print(path)
+##                    print("else: " + str(vertex.value))
+##                    path.append(vertex.value)
+##                    pathDAG(graph, vertex.value, path)
+##            else:
+##                continue  
+##            break
+##        else:
+##            continue
+##        break
+
+
+                    
 
     return path
     
-        
-            
-        
-    
-    
-    
-    
-
-    
-        
-    
-        
-
-
-    
-        
-
     
--- test_DAGfile.py
+++ test_DAGfile.py
@@ -3,30 +3,69 @@ from DAGfile import *
 
 class TestDAG(unittest.TestCase):
 
-    def test_oneNode(self):
-        #A graph with one node
-        #The LCA of one node is the node itself
-        Graph = DAGclass()
-        Graph.add("A")
-        LCA = Graph.LCA("A", "A") 
-        self.assertEqual(LCA, "A")
+##    def test_oneNode(self):
+##        #A graph with one node
+##        #The LCA of one node is the node itself
+##        Graph = DAGclass()
+##        Graph.add("A")
+##        LCA = Graph.LCA("A", "A") 
+##        self.assertEqual(LCA, "A")
+##
+##
+##    def test_oneLine(self):
+##        #A line of nodes
+##        #The LCA in a line will be the node that is the furthest up in the graph
+##        Graph = DAGclass()
+##        Graph.add("A")
+##        Graph.add("B","A")
+##        Graph.add("C","B")
+##        Graph.add("D","C")
+##        Graph.add("E","D")
+##        Graph.add("F","E")
+##        self.assertEqual(Graph.LCA("A","C"), "A")
+##        self.assertEqual(Graph.LCA("C","B"), "B")
+##        self.assertEqual(Graph.LCA("D","F"), "D")
+##
+##
+##    def test_binaryTree(self):
+##        Graph = DAGclass()
+##        Graph.add("A")
+##        Graph.add("B","A")
+##        Graph.add("C","A")
+##        Graph.add("D","B")
+##        Graph.add("E","B")
+##        Graph.add("F","C")
+##        Graph.add("G","C")
+##        self.assertEqual(Graph.LCA("D","C"), "A")
+##        self.assertEqual(Graph.LCA("G","F"), "C")
+##        self.assertEqual(Graph.LCA("B","E"), "B")
 
-
-    def test_oneLine(self):
-        #A line of nodes
-        #The LCA in a line will be the node that is the furthest up in the graph
+    def test_DAG(self):
         Graph = DAGclass()
         Graph.add("A")
         Graph.add("B","A")
         Graph.add("C","B")
-        Graph.add("D","C")
+        Graph.add("D","A")
         Graph.add("E","D")
-        Graph.add("F","E")
-        self.assertEqual(Graph.LCA("A","C"), "A")
+        Graph.add("F","C")
+        Graph.addArrow("F","E")
+        self.assertEqual(Graph.LCA("E","C"), "A")
         self.assertEqual(Graph.LCA("C","B"), "B")
-        self.assertEqual(Graph.LCA("D","F"), "D")
+        self.assertEqual(Graph.LCA("F","C"), "C")
+##        self.assertEqual(Graph.LCA("F","E"), "E")
         
+
+        
+
+##    def test_noNode(self):
+##        Graph = DAGclass()
+##        self.assertRaises()
 
 
 if __name__ == '__main__':
     unittest.main() 
+
+
+
+
+
